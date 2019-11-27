2308ca934b15b48565960aca657dbc3dac30de3b
deepak9634349532 <deepak.bansal@cygrp.com>
2019-11-27 15:21:37 +0530

    Login page

--- mobile/lib/src/screens/LoginScreen.dart
+++ mobile/lib/src/screens/LoginScreen.dart
@@ -1,21 +1,85 @@
+
+
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
 
+class _LoginScreenState extends State<LoginScreen> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
   @override
   Widget build(BuildContext context) {
+
+     
+     final emailField = TextField(
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Email",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+        );
+
+      final passwordField = TextField(
+          obscureText: true,
+          style: style,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              hintText: "Password",
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+        );
+
+    final loginButon = Material(
+          elevation: 5.0,
+          borderRadius: BorderRadius.circular(30.0),
+          color: Color(0xff01A0C7),
+          child: MaterialButton(
+            minWidth: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            onPressed: () {},
+            child: Text("Login",
+                textAlign: TextAlign.center,
+                style: style.copyWith(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+          ),
+        );
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login Screen'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Back To HomeScreen'),
-          color: Theme.of(context).primaryColor,
-          textColor: Colors.white,
-          onPressed: () => Navigator.pop(context)
-        ),
-      ),
-    );
+       title: Text('Login'),
+     ),
+          body: Center(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 155.0,
+                      child: Image.asset("assets/images/AashrayLogo.png",fit: BoxFit.cover,),
+                    ),
+                   // SizedBox(height: 45.0),
+                    emailField,
+                    SizedBox(height: 25.0),
+                    passwordField,
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                   loginButon,
+                  //  SizedBox(
+                     // height: 15.0,
+                    //),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
   }
 }
