871801aaa28436df95113f21774ef62992620625
Rishabh Singhal <rishabh.singhal@cygrp.com>
2019-12-03 00:54:10 +0530

    Added more unit tests

--- Backend/test/ngo.js
+++ Backend/test/ngo.js
@@ -69,3 +69,99 @@ describe("Update NGO Details Unit Tests", () => {
       });
   });
 });
+
+describe("Get List of NGOs", () => {
+  // #1 should return NGO's List
+  it("Should return List of NGOs", done => {
+    // calling get list api
+    server
+      .get("/api/ngo/list")
+      .expect(200) // THis is HTTP response
+      .end((err, res) => {
+          console.log(typeof res.body.data);
+        // HTTP status should be 200
+        res.status.should.equal(200);
+        // Error key should be false.
+        res.error.should.equal(false);
+        done();
+      });
+  });
+});
+
+describe("Get List of Government Shelters", () => {
+    // #1 should return Govt Shelters List
+  it("Should return List of Govt Shelters", done => {
+    // calling get list api
+    server
+      .get("/api/government/shelters")
+      .expect(200) // THis is HTTP response
+      .end((err, res) => {
+        // HTTP status should be 200
+        res.status.should.equal(200);
+        // Response should be a array of Govt Shelters
+        for(let i=0;i<res.body.data.length;i++){
+            res.body.data[i].AccomodationType.should.equal('Government_Shelters')
+        }
+        // Error key should be false.
+        res.error.should.equal(false);
+        done();
+      });
+  });
+});
+
+describe("Get Profile Details of NGO", () => {
+  // #1 should return profile detail
+  it("Should return profile details containing(userDetails, addressDetails, verificationDetails)", done => {
+    // calling get profile api
+    server
+      .get("/api/ngo/profiledetails/" + userData.validId)
+      .expect(200) // THis is HTTP response
+      .end((err, res) => {
+        // HTTP status should be 200
+        res.status.should.equal(200);
+        // Response should be contain (userDetails, addressDetails, verificationDetails)
+        res.body.data.should.have.property("userDetails");
+        res.body.data.should.have.property("addressDetails");
+        res.body.data.should.have.property("verificationDetails");
+        // Error key should be false.
+        res.error.should.equal(false);
+        done();
+      });
+  });
+
+  // #2 should return Not Found
+  it("Should contain message NGO not found", done => {
+    // calling get profile api
+    server
+      .get("/api/ngo/profiledetails/" + userData.invalidId)
+      .expect(404) // THis is HTTP response
+      .end((err, res) => {
+        // HTTP status should be 200
+        res.status.should.equal(404);
+        // Not found should be true.
+        res.notFound.should.equal(true);
+        done();
+      });
+  });
+});
+
+describe("Get List of Private properties", () => {
+  // #1 should return Private properties List
+  it("Should return List of Govt Shelters", done => {
+    // calling get list api
+    server
+      .get("/api/private/properties")
+      .expect(200) // THis is HTTP response
+      .end((err, res) => {
+        // HTTP status should be 200
+        res.status.should.equal(200);
+        // Response should be a array of Govt Shelters
+        for (let i = 0; i < res.body.data.length; i++) {
+          res.body.data[i].AccomodationType.should.equal("Private");
+        }
+        // Error key should be false.
+        res.error.should.equal(false);
+        done();
+      });
+  });
+});
--- Backend/test/user.js
+++ Backend/test/user.js
@@ -69,3 +69,75 @@ describe("Update User Details Unit Tests", () => {
       });
   });
 });
+
+describe("Get Profile Details of User", () => {
+  // #1 should return profile detail
+  it("Should return profile details containing(userDetails, addressDetails, verificationDetails)", done => {
+    // calling get profile api
+    server
+      .get("/api/user/profiledetails/" + userData.validId)
+      .expect(200) // THis is HTTP response
+      .end((err, res) => {
+        // HTTP status should be 200
+        res.status.should.equal(200);
+        // Response should be contain (userDetails, addressDetails, verificationDetails)
+        res.body.data.should.have.property("userDetails");
+        res.body.data.should.have.property("addressDetails");
+        res.body.data.should.have.property("verificationDetails");
+        // Error key should be false.
+        res.error.should.equal(false);
+        done();
+      });
+  });
+
+  // #2 should return Not Found
+  it("Should contain message User not found", done => {
+    // calling get profile api
+    server
+      .get("/api/user/profiledetails/" + userData.invalidId)
+      .expect(404) // THis is HTTP response
+      .end((err, res) => {
+        // HTTP status should be 200
+        res.status.should.equal(404);
+        // Error key should be false.
+        res.notFound.should.equal(true);
+        done();
+      });
+  });
+});
+
+describe("Get List of User Offered Helps", () => {
+  // #1 should return Private properties List
+  it("Should return List of User Offered Helps", done => {
+    // calling get list api
+    server
+      .get("/api/user/offeredhelp/"+userData.validId )
+      .expect(200) // THis is HTTP response
+      .end((err, res) => {
+        // HTTP status should be 200
+        res.status.should.equal(200);
+        // Response should be a array of private properties
+        for (let i = 0; i < res.body.data.length; i++) {
+          res.body.data[i].AccomodationType.should.equal("Private");
+        }
+        // Error key should be false.
+        res.error.should.equal(false);
+        done();
+      });
+  });
+
+    // #1 should return not found
+  it("Should return not found", done => {
+    // calling get list api
+    server
+      .get("/api/user/offeredhelp/"+userData.invalidId )
+      .expect(404) // THis is HTTP response
+      .end((err, res) => {
+        // HTTP status should be 200
+        res.status.should.equal(404);
+        // Not found should be true.
+        res.notFound.should.equal(true);
+        done();
+      });
+  });
+});
